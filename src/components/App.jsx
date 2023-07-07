cef3603a5f5a14008d55ceebe982eb1c6cc46b32
Renita <ira3624@icloud.com>
2023-07-07 20:35:32 +0300

    hw-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
+import { useState, useEffect } from 'react';
+import { ContactList } from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { nanoid } from 'nanoid';
+
+export function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const contacts = localStorage.getItem('contacts');
+    const array = JSON.parse(contacts);
+    if (array) {
+      setContacts(array);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const formSubmitHandler = data => {
+    const newData = { ...data, id: nanoid() };
+    const nameFilter = contacts.filter(contact =>
+      contact.name.includes(data.name)
+    );
+    if (nameFilter.length > 0) {
+      alert(`${data.name} is already in contacts`);
+    } else {
+      setContacts(prevState => [newData, ...prevState]);
+    }
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const normalizedFilter = filter.toLowerCase();
+  const visubleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={formSubmitHandler} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList contacts={visubleContacts} onDeleteContact={deleteContact} />
     </div>
   );
-};
+}
